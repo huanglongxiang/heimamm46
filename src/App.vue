fee20f9f779f9e3177c1eff157b852c95afcf524
huanglongxiang <2714161480@qq.com>
2020-02-11 23:16:16 +0800

    element引入

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-button type="success">测试按钮</el-button>
+   <el-button type="success">成功按钮</el-button>
 </template>
 
 <script>
@@ -11,3 +11,5 @@ export default {
 <style>
 
 </style>
+
+
