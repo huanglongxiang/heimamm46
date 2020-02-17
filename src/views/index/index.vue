e3f63b719ae24f44fff1cae47759f8a406db6f5a
huanglongxiang <2714161480@qq.com>
2020-02-17 11:14:03 +0800

    注册 路由整合 左侧导航与右侧内容进行联动

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -16,24 +16,24 @@
     </el-header>
     <el-container>
       <el-aside width="auto">
-        <el-menu default-active="2" class="el-menu-vertical-demo" :collapse="isCollapse">
-          <el-menu-item index="1">
+        <el-menu router :default-active="$route.path" class="el-menu-vertical-demo" :collapse="isCollapse">
+          <el-menu-item index="/index/chart">
             <i class="el-icon-pie-chart"></i>
             <span slot="title">数据概览</span>
           </el-menu-item>
-          <el-menu-item index="2">
+          <el-menu-item index="/index/user">
             <i class="el-icon-user"></i>
             <span slot="title">用户列表</span>
           </el-menu-item>
-          <el-menu-item index="3">
+          <el-menu-item index="/index/question">
             <i class="el-icon-edit-outline"></i>
             <span slot="title">题库列表</span>
           </el-menu-item>
-          <el-menu-item index="4">
+          <el-menu-item index="/index/enterprise">
             <i class="el-icon-office-building"></i>
             <span slot="title">企业列表</span>
           </el-menu-item>
-          <el-menu-item index="5">
+          <el-menu-item index="/index/subject">
             <i class="el-icon-notebook-2"></i>
             <span slot="title">学科列表</span>
           </el-menu-item>
@@ -60,13 +60,12 @@ export default {
   created() {
     // 读取存储的 token
     getInfo().then(res => {
-      window.console.log(res);
       if (res.data.code === 200) {
         this.userlogin = res.data.data;
-        this.userlogin.avatar =
-          process.env.VUE_APP_URL + "/" + this.userlogin.avatar;
+        this.userlogin.avatar = process.env.VUE_APP_URL + "/" + this.userlogin.avatar;
       }
     });
+    window.console.log(this.$route.path)
   },
   methods: {
     // 注销逻辑
